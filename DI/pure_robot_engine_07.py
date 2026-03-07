a5379c4f610837cc28bf359af28185d0a6eb405d
c44o <149115105+c44o@users.noreply.github.com>
2026-03-07 22:53:54 +0300

    Implement PureRobotEngine with movement methods

---
+++ DI/pure_robot_engine_07.py
@@ -0,0 +1,18 @@
+import pure_robot
+from engine_protocol_07 import RobotEngine, Transfer
+
+class PureRobotEngine(RobotEngine[pure_robot.RobotState]):
+    def move(self, transfer: Transfer, distance: int, state: pure_robot.RobotState):
+        return pure_robot.move(transfer, distance, state)
+    
+    def turn(self, transfer: Transfer, angle: int, state: pure_robot.RobotState):
+        return pure_robot.turn(transfer, angle, state)
+    
+    def set_state(self, transfer: Transfer, mode: str, state: pure_robot.RobotState):
+        return pure_robot.set_state(transfer, mode, state)
+    
+    def start(self, transfer: Transfer, state: pure_robot.RobotState):
+        return pure_robot.start(transfer, state)
+    
+    def stop(self, transfer: Transfer, state: pure_robot.RobotState):
+        return pure_robot.stop(transfer, state)
