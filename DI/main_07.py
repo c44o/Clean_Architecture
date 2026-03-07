f9b5a4a1cb05b556909ad846cd465bf9d7f9696b
c44o <149115105+c44o@users.noreply.github.com>
2026-03-07 23:01:05 +0300

    Update import for CleanerApi to cleaner_api_07

--- DI/main_07.py
+++ DI/main_07.py
@@ -1,5 +1,5 @@
 from pure_robot_engine_07 import PureRobotEngine
-from cleaner_api import CleanerApi
+from cleaner_api_07 import CleanerApi
 
 api = CleanerApi(PureRobotEngine())
 api.activate_cleaner(("move 100", "turn -90", "set soap", "start", "move 50", "stop"))
