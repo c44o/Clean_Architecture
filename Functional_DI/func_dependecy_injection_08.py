0c8dc412b69ce8faf8045860ee8fe64213ef16c2
c44o <149115105+c44o@users.noreply.github.com>
2026-03-10 20:40:21 +0300

    Update func_dependecy_injection_08.py

--- Functional_DI/func_dependecy_injection_08.py
+++ Functional_DI/func_dependecy_injection_08.py
@@ -1,1 +1,40 @@
+from __future__ import annotations
+from typing import Callable, Any
 
+State = Any
+Transfer = Callable[[Any], None]
+
+Move_functional = Callable[[Transfer, int, State], State]
+Turn_functional = Callable[[Transfer, int, State], State]
+Set_functional = Callable[[Transfer, str, State], State]
+Start_functional = Callable[[Transfer, State], State]
+Stop_functional = Callable[[Transfer, State], State]
+
+def run_program(
+        transfer: Transfer, 
+        code_to_execute: tuple[str, ...],
+        state: State,
+        move_functional: Move_functional,
+        turn_functional: Turn_functional,
+        set_functional: Set_functional,
+        start_functional: Start_functional,
+        stop_functional: Stop_functional
+) -> State:
+    for line in code_to_execute:
+        parts = line.split()
+        option = parts[0].lower()
+
+        if option == "move":
+            state = move_functional(transfer, int(parts[1]), state)
+        elif option == "turn":
+            state = turn_functional(transfer, int(parts[1]), state)
+        elif option == "set":
+            state = set_functional(transfer, parts[1], state)
+        elif option == "start":
+            state = start_functional(transfer, state)
+        elif option == "stop":
+            state = stop_functional(transfer, state)
+        else:
+            raise ValueError(f"{option}- какая-то непонятная команда")
+        
+    return state
