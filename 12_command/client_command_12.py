e5f15534cdd086214d0063bc056bdf3167e812fb
c44o <149115105+c44o@users.noreply.github.com>
2026-03-22 23:14:38 +0300

    Create client_command_12.py

---
+++ 12_command/client_command_12.py
@@ -0,0 +1,23 @@
+import pure_robot
+from command_robot_12 import MoveCommand, TurnCommand, SetCommand, StartCommand, StopCommand, CommandExecutor
+
+def main():
+    commands = [
+        MoveCommand(100),
+        TurnCommand(-90),
+        SetCommand("soap"),
+        StartCommand(),
+        MoveCommand(50),
+        StopCommand(),
+    ]
+
+
+    executor = CommandExecutor()
+    final_state = executor.execute_all(commands)
+
+    print("program finished")
+    print("final state: ", final_state)
+
+
+if __name__ == "__main__":
+    main()
