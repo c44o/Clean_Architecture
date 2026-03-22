f9c125d594c4c4c3d41875c4c95de7be79a78754
c44o <149115105+c44o@users.noreply.github.com>
2026-03-22 23:14:52 +0300

    Update command_robot_12.py

--- 12_command/command_robot_12.py
+++ 12_command/command_robot_12.py
@@ -1,15 +1,58 @@
 import pure_robot
 
 class Command:
+    def execute(self, transfer, state):
+        raise NotImplementedError("в интерфесе метод обозначен, а не реализован")
+    
 
-class MoveCommand:
+class MoveCommand(Command):
+    def __init__(self, distance):
+        self.distance = distance
 
-class TurnCommand:
+    def execute(self, transfer, state):
+        return pure_robot.move(transfer, self.distance, state)
+    
 
-class SetCommand:
+class TurnCommand(Command):
+    def __init__(self, angle):
+        self.angle = angle
 
-class StartCommand:
+    def execute(self, transfer, state):
+        return pure_robot.turn(transfer, self.angle, state)
+    
 
-class StopCommand:
+class SetCommand(Command):
+    def __init__(self, mode):
+        self.mode = mode
 
+    def execute(self, transfer, state):
+        return pure_robot.set_state(transfer, self.mode, state)
+
+class StartCommand(Command):
+    def execute(self, transfer, state):
+        return pure_robot.start(transfer, state)
+    
+
+class StopCommand(Command):
+    def execute(self, transfer, state):
+        return pure_robot.stop(transfer, state)
+    
 class CommandExecutor:
+    def __init__(self, transfer=None, initial_state=None):
+        if transfer is None:
+            transfer = pure_robot.transfer_to_cleaner
+
+        if initial_state is None:
+            initial_state = pure_robot.RobotState(0.0, 0.0, 0, pure_robot.WATER)
+
+        self.transfer = transfer 
+        self.initial_state = initial_state
+
+    def execute_all(self, commands):
+        state = self.initial_state
+
+        for command in commands:
+            state = command.execute(self.transfer, state)
+
+        return state
+    
