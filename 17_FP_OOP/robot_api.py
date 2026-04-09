ca35231847c15de444ea7ffc53ef02e093fcd8ed
c44o <149115105+c44o@users.noreply.github.com>
2026-04-09 23:16:13 +0300

    Update robot_api.py

--- 17_FP_OOP/robot_api.py
+++ 17_FP_OOP/robot_api.py
@@ -1,1 +1,21 @@
-#pass
+from robot_impl import *
+
+robot0 = Robot.create()
+print(robot0)
+
+move_result = robot0.move(100)
+robot1 = move_result.robot
+print(move_result)
+print(robot1)
+
+robot2 = robot1.turn(-90)
+print(robot2)
+
+mode_result = robot2.set_mode(CleaningMode.SOAP)
+robot3 = mode_result.robot
+print(mode_result)
+print(robot3)
+
+robot4 = robot3.move(50).robot
+print(robot4)
+print("Исходный robot0 не остался как был", robot0)
